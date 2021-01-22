0a182ab701cc6907340fcb1acdd6da00f4ef445b
Bahri KOÇ <61977415+Bahri26@users.noreply.github.com>
2021-01-22 22:19:44 +0300

    Add files via upload

---
+++ AddList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddMethod
+{
+    class AddList<N,D>
+    {
+        List<N> _values;
+        List<D> _days ;
+
+        public AddList()
+        {
+            _values = new List<N>();
+            _days = new List<D>();
+        }
+
+        public void Add(N _values,D _days)
+        {
+            Console.WriteLine("{0}. gün = {1}", _values, _days);
+        }
+
+        public int Count
+        {
+            get { return _days.Count;}
+        }
+    }
+
+    
+}
