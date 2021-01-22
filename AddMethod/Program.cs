d47314a37091dff0b899010737bb1608272410e6
Bahri KOÇ <61977415+Bahri26@users.noreply.github.com>
2021-01-22 22:28:10 +0300

    Add files via upload

---
+++ AddMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AddMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddList<int,String> days = new AddList<int, String>();
+            days.Add(1,"Monday");
+            days.Add(2,"Tuesday");
+            days.Add(3,"Wednesday");
+            days.Add(4,"Thursday");
+            days.Add(5,"Friday");
+            days.Add(6,"Saturday");
+            days.Add(7,"Sunday");
+
+            for (int i = 0; i < days.Count-1; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    
+}
