474fd0768571431de682bac345133e1f72388750
Bahri KOÇ <61977415+Bahri26@users.noreply.github.com>
2021-01-31 17:05:19 +0300

    Add files via upload

---
+++ ReCap Project Car/ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("{0} Araba günlük fiyatı = {1}", car.BrandId, car.DailyPrice);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
