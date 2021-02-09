3c485d7265bbec5b308a1709156f19a9e2772d6e
Bahri KOÇ <61977415+Bahri26@users.noreply.github.com>
2021-02-09 21:54:54 +0300

    Add files via upload

---
+++ CarRental/Business/Concrete/CarManager.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandId == id);
+        }
+
+        public List<Car> GetByDAilyPrice(decimal min, decimal max)
+        {
+            return _carDal.GetAll(p => p.DailyPrice >= min && p.DailyPrice <= max);
+        }
+
+        public List<CarDetailsDto> GetCarDetails()
+        {
+            return _carDal.GetCarDetails();
+        }
+    }
+}
---
+++ CarRental/DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,36 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : EfEntityRepositoryBase<Car, CarContext>, ICarDal
+    {
+        public List<CarDetailsDto> GetCarDetails()
+        {
+            using (CarContext context = new CarContext())
+            {
+                var result = from car in context.Car
+                             join color in context.Colors
+                             on car.ColorId equals color.Id
+                             join brand in context.Brands
+                             on car.BrandId equals brand.Id
+                             select new CarDetailsDto
+                             {
+                                 CarName = car.CarName,
+                                 BrandName = brand.Name,
+                                 ColorName = color.Name,
+                                 DailyPrice = car.DailyPrice
+                             };
+                return result.ToList();
+            }
+        }
+    }
+}
